b816790a9205b703d05a79be24f483f4185b3f14
Steven Bowler <stevenbowler@yahoo.com>
2020-06-23 18:20:47 -0500

    add tooltips to animation on Home.js

--- client/src/components/Animations/Animations.js
+++ client/src/components/Animations/Animations.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { bounce, pulse, zoomIn, fadeIn } from "react-animations";   // flipInX, flipInY see https://www.npmjs.com/package/react-animations
 import styled, { keyframes } from 'styled-components';
+import { Tooltip } from "reactstrap";
 import './style.css';
 
 
@@ -26,121 +27,148 @@ class AnimateBounce extends Component {
     }
 }
 
-
+/**
+ *       <Tooltip placement="right" isOpen={tooltipOpen} target="TooltipExample" toggle={toggle}>
+        Hello world!
+      </Tooltip>
+ */
 
 /**Animation wobble effect from react-animations
  * @class
  */
-class AnimateZoomIn extends Component {
-    render() {
-        return (
-            <ZoomIn>
-                {/* <a href={this.props.href}><img src={this.props.src} alt='Menu' style={{
-                    height,
-                    width
-                }}></img>
-                </a> */}
-                <div style={{ color: "white" }}>
-                    {/* <h2>About Me</h2> */}
-                    <section>
-                        <img src="/IMG_3192.JPG" alt="SB photo" height="100px"
-                            width="160px"></img>
-                        <p ><strong> Husband to Laura, father to Alan and Eva - and we have 7
+const AnimateZoomIn = (props) => {
+
+    // setTooltipOpen = useState(false);
+    const toggle = () => setTooltipOpen(!tooltipOpen);
+    const [tooltipOpen, setTooltipOpen] = useState(false);
+
+    // render() {
+    return (
+        <ZoomIn>
+            <div style={{ color: "white" }} >
+                {/* <h2>About Me</h2> */}
+                <section>
+                    <img src="/IMG_3192.JPG" alt="SB photo" height="100px"
+                        width="160px"></img>
+                    <p ><strong> Husband to Laura, father to Alan and Eva - and we have 7
                     dogs too.</strong>
-                        </p><br></br>
-                        <p >I offer, proven project management and technical skill, leadership, good communication, and above all, a successful addition to your team.
                     </p><br></br>
-                        <p > Starting January 2020 I am enrolled in both: Masters
-                        Science in Information Technology at University of Texas Rio Grande Valley; and, the Coding Bootcamp
-                        program at University
-                        of Texas Austin. Professional development in these areas will complement my hands-on experience and
-                        enable me to provide good solutions to business problems.
+                    <p >I offer, proven project management and technical skill, leadership, good communication, and above all, a successful addition to your team.
                     </p><br></br>
-                        <p > There are significant opportunities to solve customer or issues
-                        through project management and software development.
+                    <p > Starting January 2020 I am enrolled in both: Masters
+                    Science in Information Technology at University of Texas Rio Grande Valley; and, the Coding Bootcamp
+                    program at University
+                    of Texas Austin. Professional development in these areas will complement my hands-on experience and
+                    enable me to provide good solutions to business problems.
                     </p><br></br>
-                    </section>
-                </div>
-            </ZoomIn>
-        );
-    }
+                    <p > There are significant opportunities to solve customer or issues
+                    through project management and software development.
+                    </p><br></br>
+                </section>
+            </div>
+        </ZoomIn>
+    );
 }
+// }
 
 
 /**Animation wobble effect from react-animations
  * @class
  */
-class AnimateFadeIn extends Component {
-    render() {
-        return (
-            <FadeIn>
-                {/* <a href={this.props.href}><img src={this.props.src} alt='Menu' style={{
-                    height,
-                    width
-                }}></img>
-                </a> */}
-                <div style={{ color: "white", marginLeft: "100px", fontSize: "1.5em" }}>
-                    {/* <h2>About Me</h2> */}
-                    <section>
+const AnimateFadeIn = (props) => {
+    // render() {
+    return (
+        <FadeIn>
+            <div style={{ color: "white", marginLeft: "100px", fontSize: "1.5em" }}>
+                {/* <h2>About Me</h2> */}
+                <section>
 
-                        <p ><strong> Best way to contact me is to text your name to 1-956-792-4128</strong></p>
-                        <p >Alternatively of course, call the same number.</p>
-                        <p >Or send an email to stevenbowler@yahoo.com</p>
-                        <p >It will be my pleasure to help you and your team reach your goals.</p>
-                        <p >Sincerely, </p>
-                        <p >Steven Bowler</p>
-                        <p >stevenbowler@yahoo.com</p>
-                        <p >1-956-792-4128</p>
-                        <br></br>
-                    </section>
-                </div>
-            </FadeIn>
-        );
-    }
+                    <p ><strong> Best way to contact me is to text your name to 1-956-792-4128</strong></p>
+                    <p >Alternatively of course, call the same number.</p>
+                    <p >Or send an email to stevenbowler@yahoo.com</p>
+                    <p >It will be my pleasure to help you and your team reach your goals.</p>
+                    <p >Sincerely, </p>
+                    <p >Steven Bowler</p>
+                    <p >stevenbowler@yahoo.com</p>
+                    <p >1-956-792-4128</p>
+                    <br></br>
+                </section>
+            </div>
+        </FadeIn>
+    );
 }
+// }
 
 
 /**Animation wobble effect from react-animations
  * @class
  */
-class AnimateWobble extends Component {
-    render() {
-        return (
-            <Wobble>
-                <a href={this.props.href}><img src={this.props.src} alt='Menu' style={{
-                    height,
-                    width
-                }}></img>
-                </a>
-            </Wobble>
-        );
-    }
+const AnimateWobble = (props) => {
+
+    const toggle = () => setTooltipOpen(!tooltipOpen);
+    const [tooltipOpen, setTooltipOpen] = useState(false);
+
+    // render() {
+    return (
+        <Wobble>
+            <Tooltip placement="right" isOpen={tooltipOpen} target="TooltipWobble" toggle={toggle}>
+                Click to enter the Clicky One game.  Test your short term memory by clicking on the images, taking care
+                not to click the same image twice, that will end the game.  Register and log in and have your scores noted
+                on the top 5 board.
+            </Tooltip>
+            <a id="TooltipWobble" href={props.href}><img src={props.src} alt='Menu' style={{
+                height,
+                width
+            }}></img>
+            </a>
+        </Wobble>
+    )
+    // {/* } */ }
 }
 
 
 
-class AnimateRotateInDownLeft extends Component {
-    render() {
-        return (
-            <RotateInDownLeft><a style={{ color: "white" }} href={this.props.href}><img src={this.props.src} alt='Menu' style={{
+const AnimateRotateInDownLeft = (props) => {
+    const toggle = () => setTooltipOpen(!tooltipOpen);
+    const [tooltipOpen, setTooltipOpen] = useState(false);
+
+    // render() {
+    return (
+        <RotateInDownLeft>
+            <Tooltip placement="right" isOpen={tooltipOpen} target="TooltipRotateInDownLeft" toggle={toggle}>
+                Click to enter the Jiggle React Game.  Use the left/right/up/down keys to move the BlackBox around
+                the Texas Longhorns colored gamespace.  Don't let the Fuel Thief touch you he will drain your fuel.
+                Stop on top of the React fuel tank as much as possible to fuel up.
+            </Tooltip>
+
+            <a id="TooltipRotateInDownLeft" style={{ color: "white" }} href={props.href}><img src={props.src} alt='Menu' style={{
                 height,
                 width
             }}></img></a></RotateInDownLeft>
-        )
-    }
+    )
 }
 
 
-class AnimateRotateInDownRight extends Component {
-    render() {
-        return (
-            <RotateInDownRight><a href={this.props.href}><img src={this.props.src} alt='Menu' style={{
+const AnimateRotateInDownRight = (props) => {
+    const toggle = () => setTooltipOpen(!tooltipOpen);
+    const [tooltipOpen, setTooltipOpen] = useState(false);
+
+    // render() {
+    return (
+        <RotateInDownRight>
+            <Tooltip placement="right" isOpen={tooltipOpen} target="TooltipRotateInDownRight" toggle={toggle}>
+                Click to enter the Google Book Search. From the home page save any notes you
+                wish to your Saved list. Search Google Books by entering any search string from
+                the Search page, then add a book to your Saved books.  Register and log
+                in to maintain your own personal list.
+            </Tooltip>
+            <a id="TooltipRotateInDownRight" href={props.href}><img src={props.src} alt='Menu' style={{
                 color: "white",
                 height,
                 width
-            }}></img></a></RotateInDownRight>
-        )
-    }
+            }}></img></a></RotateInDownRight >
+    )
+    // }
 }
 
 export { AnimateWobble, AnimateRotateInDownLeft, AnimateRotateInDownRight, AnimateZoomIn, AnimateFadeIn }
